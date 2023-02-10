96dd11600a90bef6332f35d1e6b036da0df582c5
PhaserEditor2D <developers@phasereditor2d.com>
2023-02-10 06:13:39 -0500

    First version.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+rm -Rf $PHASEREDITOR_HOME/PhaserEditor2D-v3-beta-docs-html/docs
+
+# build docs
+
+cd $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs
+./build.sh
+cp -R _build/html $PHASEREDITOR_HOME/PhaserEditor2D-v3-beta-docs-html/
+
+# clean
+
+cd $PHASEREDITOR_HOME/PhaserEditor2D-v3-beta-docs-html/
+mv html docs
+rm -R docs/_sources
+rm -R docs/.doctrees
+cp _config.yml.template docs/_config.yml
